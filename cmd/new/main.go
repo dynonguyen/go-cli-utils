b3364ab61fe1a766710105516e9029813f365870
Nguyen Le Anh Tuan <tuannla5@fpt.com>
2024-06-09 18:56:34 +0700

    Release v1.0.0

--- cmd/new/main.go
+++ cmd/new/main.go
@@ -1,7 +1,26 @@
 package main
 
-import newcli "github.com/dynonguyen/new-cli"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	newcli "github.com/dynonguyen/new-cli"
+)
 
 func main() {
-	newcli.Run()
+	var verbose bool
+
+	flag.BoolVar(&verbose, "verbose", false, "Verbose output")
+	flag.BoolVar(&verbose, "v", false, "Verbose output")
+
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "No paths provided\n")
+		os.Exit(1)
+	}
+
+	newcli.NewCli(args, verbose)
 }
